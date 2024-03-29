bc8dd9cf938f450f8130341fc0cfdf88b2d502e2
MaksymP <maximus-ms@ukr.net>
2024-03-29 20:15:45 +0000

    Task1

--- website_status.sh
+++ website_status.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-urls=('https://www.google.com',
-    'https://www.facebook.com',
-    'https://www.twitter.com'
-)
+urls=('https://www.google.com' 'https://www.facebook.com' 'https://www.twitter.com')
 
-log_file='/tmp/website_status.log'
+log_file='./website_status.log'
 
 for url in "${urls[@]}"; do
-    curl -s -L --head --request GET "<https://www.example.com>" | grep "HTTP/2 200"
-    if curl --output /dev/null --silent --head --fail "$url"; then
+    if curl -s -L --head --request GET "$url" | grep "HTTP/2 200" > /dev/null; then
+        echo "$url is UP"
         echo "$url is UP" >> "$log_file"
     else
+        echo "$url is DOWN"
         echo "$url is DOWN" >> "$log_file"
     fi
 done
 
-echo "Log file: $log_file"
+echo "Results are in a log file: $log_file"
